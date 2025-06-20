71563e1eb9539c0c0dff37b4a06e3840b31c9dfe
guilhermecastro <guilherme.castro@protonmail.com>
2025-06-20 10:54:10 -0300

    feat: add license headers to Python files and create license header script

---
+++ add_license.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+for i in $(find ./ -name '*.py');  # or whatever other pattern...
+do
+  if ! grep -q Copyright $i
+  then
+    cat LICENSE_HEADER $i >$i.new && mv $i.new $i
+  fi
+done
