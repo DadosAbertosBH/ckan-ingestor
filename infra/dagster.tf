4c7b192667d80824c7dac065a1bdcd7ab6ef3588
guilhermecastro <guilherme.castro@protonmail.com>
2025-06-23 16:35:03 -0300

    fix: update deployment configuration in dagster.tf to include dagsterDaemon and dagsterWebserver images

--- infra/dagster.tf
+++ infra/dagster.tf
@@ -5,55 +5,75 @@ resource "helm_release" "dagster" {
   namespace        = "dagster"
   create_namespace = true
 
-  values = [yamlencode(
-    {
-      dagster-user-deployments = {
-        deployments = [{
-          name = "ckan-pbh"
-          port = 3030
+  values = [
+    yamlencode(
+      {
+        dagsterDaemon = {
+          image = {
+            name       = "pedalin/dagster-celery-k8s"
+            repository = "registry.gitlab.com"
+            tag        = "4ecbc1ce"
+            pullPolicy = "IfNotPresent"
+          }
+        }
+        dagsterWebserver = {
           image = {
             name       = "pedalin/ckan-ingestor"
             repository = "registry.gitlab.com"
-            tag        = var.image_tag
+            tag        = "4ecbc1ce"
             pullPolicy = "IfNotPresent"
           }
+        }
+        dagster-user-deployments = {
+          deployments = [
+            {
+              name = "ckan-pbh"
+              port = 3030
+              image = {
+                name       = "pedalin/ckan-ingestor"
+                repository = "registry.gitlab.com"
+                tag        = var.image_tag
+                pullPolicy = "IfNotPresent"
+              }
 
-          env = [
-            {
-              name  = "DUCKLAKE_CATALOG_URI"
-              value = "postgres:dbname=mysql host=${var.ducklake_db_host} database=${var.ducklake_db_database} user=${var.ducklake_db_user} password=${var.ducklake_db_password}"
-            },
-            {
-              name  = "DUCKLAKE_DATABASE"
-              value = ":memory:"
-            },
-            {
-              name  = "DUCKLAKE_DATA_PATH__ENDPOINT"
-              value = var.s3_endpoint
-            },
-            {
-              name  = "DUCKLAKE_DATA_PATH__URL_STYLE"
-              value = "path"
-            },
-            {
-              name  = "DUCKLAKE_DATA_PATH__ACCESS_KEY_ID"
-              value = var.s3_access_key
-            },
-            {
-              name  = "DUCKLAKE_DATA_PATH__SECRET_ACCESS_KEY"
-              value = var.s3_secret_key
-              }, {
-              name  = "DUCKLAKE_DATA_PATH__BUCKET"
-              value = "public-datasets"
-            },
-          ]
+              env = [
+                {
+                  name  = "DUCKLAKE_CATALOG_URI"
+                  value = "postgres:dbname=mysql host=${var.ducklake_db_host} database=${var.ducklake_db_database} user=${var.ducklake_db_user} password=${var.ducklake_db_password}"
+                },
+                {
+                  name  = "DUCKLAKE_DATABASE"
+                  value = ":memory:"
+                },
+                {
+                  name  = "DUCKLAKE_DATA_PATH__ENDPOINT"
+                  value = var.s3_endpoint
+                },
+                {
+                  name  = "DUCKLAKE_DATA_PATH__URL_STYLE"
+                  value = "path"
+                },
+                {
+                  name  = "DUCKLAKE_DATA_PATH__ACCESS_KEY_ID"
+                  value = var.s3_access_key
+                },
+                {
+                  name  = "DUCKLAKE_DATA_PATH__SECRET_ACCESS_KEY"
+                  value = var.s3_secret_key
+                }, {
+                  name  = "DUCKLAKE_DATA_PATH__BUCKET"
+                  value = "public-datasets"
+                },
+              ]
 
-          dagsterApiGrpcArgs = [
-            "--python-file",
-            "/app/ckan_dagster/ckan_dagster/definitions.py"
+              dagsterApiGrpcArgs = [
+                "--python-file",
+                "/app/ckan_dagster/ckan_dagster/definitions.py"
+              ]
+            }
           ]
-        }]
+        }
       }
-    }
-  )]
+    )
+  ]
 }
