36a24b116a2624148572dddc48be064cebf75113
guilhermecastro <guilherme.castro@protonmail.com>
2025-07-03 14:05:42 -0300

    fix: enable in-process executor for job definitions

--- ckan_dagster/ckan_dagster/definitions.py
+++ ckan_dagster/ckan_dagster/definitions.py
@@ -145,6 +145,7 @@ def ckan_data_request_sensor(
 ducklake_settings = DucklakeSettingsResource()
 metadata_ingestor = DuckdbMetadataIngestorResource(ducklake_settings=ducklake_settings)
 defs = dg.Definitions(
+    executor=dg.in_process_executor,
     assets=[ckan_datasets, ckan_resources, ckan_data],
     jobs=[ckan_data_job],
     sensors=[ckan_data_request_sensor],
