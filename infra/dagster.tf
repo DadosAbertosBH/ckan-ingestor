18eacbede4f9619df78e83d6264f1c3c1546a728
guilhermecastro <guilherme.castro@protonmail.com>
2025-07-04 18:31:20 -0300

    update chart version

--- infra/dagster.tf
+++ infra/dagster.tf
@@ -91,7 +91,7 @@ resource "helm_release" "dagster" {
   chart            = "dagster"
   namespace        = "dagster"
   create_namespace = true
-  version          = "0.0.5-dev"
+  version          = "0.0.6-dev"
 
   values = [
     yamlencode(
