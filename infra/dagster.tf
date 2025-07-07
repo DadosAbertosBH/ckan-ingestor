09c12115857834c6d5f6bccb21cb49d86a43567e
guilhermecastro <guilherme.castro@protonmail.com>
2025-07-06 23:14:32 -0300

    set 4 replicas

--- infra/dagster.tf
+++ infra/dagster.tf
@@ -140,7 +140,7 @@ resource "helm_release" "dagster" {
               image           = local.dagster_image
               workerQueues = [{
                 name         = "dagster"
-                replicaCount = 5
+                replicaCount = 4
               }]
               envSecrets = [
                 {
@@ -149,11 +149,11 @@ resource "helm_release" "dagster" {
               ]
               resources = {
                 requests = {
-                  cpu    = "500m"
+                  cpu    = "600m"
                   memory = "3Gi"
                 }
                 limits = {
-                  cpu    = "500m"
+                  cpu    = "600m"
                   memory = "3Gi"
                 }
               }
