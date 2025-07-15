84254eb038430803764117dcf7f6049b237662b6
guilhermecastro <guilherme.castro@protonmail.com>
2025-07-15 17:58:51 -0300

    use mysql database

--- infra/dagster.tf
+++ infra/dagster.tf
@@ -50,7 +50,7 @@ resource "kubernetes_secret" "ingest_secret" {
 
 
   data = {
-    "DUCKLAKE_CATALOG_URI"                  = "postgres:host=${var.ducklake_db_host} dbname=${var.ducklake_db_database} user=${var.ducklake_db_user} password=${var.ducklake_db_password}"
+    "DUCKLAKE_CATALOG_URI"                  = "mysql:host=${var.ducklake_db_host} db=${var.ducklake_db_database} user=${var.ducklake_db_user} password=${var.ducklake_db_password}"
     "DUCKLAKE_DATA_PATH__ACCESS_KEY_ID"     = var.s3_access_key
     "DUCKLAKE_DATA_PATH__ENDPOINT"          = var.s3_endpoint
     "DUCKLAKE_DATA_PATH__BUCKET"            = "public-datasets"
