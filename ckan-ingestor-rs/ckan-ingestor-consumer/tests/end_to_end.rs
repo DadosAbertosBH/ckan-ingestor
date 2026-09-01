c9f8b493d7875b3b77329a9617cec33f9d7e7b80
guilhermecastro <guilherme.castro@protonmail.com>
2026-09-01 11:00:16 -0300

    try fix port

--- ckan-ingestor-rs/ckan-ingestor-consumer/tests/end_to_end.rs
+++ ckan-ingestor-rs/ckan-ingestor-consumer/tests/end_to_end.rs
@@ -17,7 +17,7 @@ use tokio::time::{Duration, timeout};
 
 async fn start_iggy() -> anyhow::Result<ContainerAsync<GenericImage>> {
     Ok(GenericImage::new("apache/iggy", "0.8.0")
-        .with_exposed_port(8090.tcp())
+        .with_mapped_port(0, 8090.tcp())
         .with_security_opt("seccomp=unconfined")
         .with_env_var("IGGY_ROOT_USERNAME", "iggy")
         .with_env_var("IGGY_ROOT_PASSWORD", "iggy")
