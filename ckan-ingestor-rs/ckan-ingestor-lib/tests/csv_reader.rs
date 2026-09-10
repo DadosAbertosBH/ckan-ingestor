f8264687ccccb36353d2afb71a79b642a263beab
guilhermecastro <guilherme.castro@protonmail.com>
2026-09-10 18:17:53 -0300

    test: align CSV sample expectation

--- ckan-ingestor-rs/ckan-ingestor-lib/tests/csv_reader.rs
+++ ckan-ingestor-rs/ckan-ingestor-lib/tests/csv_reader.rs
@@ -218,7 +218,7 @@ fn csv_with_bom() -> Result<()> {
     assert_eq!(result.encoding.as_deref(), Some("UTF-8"));
     assert_eq!(result.csv_strict_mode, Some(true));
     assert_eq!(result.csv_delimiter.as_deref(), Some(","));
-    assert_eq!(result.csv_samples.as_deref(), Some("50000"));
+    assert_eq!(result.csv_samples.as_deref(), Some("25000"));
     Ok(())
 }
 
